6c74152c3a8979e47352e215d017167f2a405d26
Sergey Cheparev <sergey@cheparev.com>
2018-07-12 09:46:47 +0200

    Fixed some formatting

--- bullshit/webapp/webapp.py
+++ bullshit/webapp/webapp.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def get_horoscope():
-	return render_template("horoscope.html", horoscope=horoscope.generate())
+    return render_template("horoscope.html", horoscope=horoscope.generate())
+
 
 @app.route("/dirty/")
 def get_dirty_horoscope():
-	return render_template("horoscope.html", horoscope=horoscope.generate(dirty=True))
+    return render_template("horoscope.html", horoscope=horoscope.generate(dirty=True))
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
-
+    app.run(debug=True)
